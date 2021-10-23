37049ccd6c84ef04504badf6342a25beea2a6478
ramel0915 <sdy992000@kaist.ac.kr>
2021-10-23 17:47:44 +0900

    6.4Redirecting

---
+++ movie_app_2021/src/components/Navigation.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {Link} from "react-router-dom"
+import "./Navigation.css";
+import style from "styled-components";
+
+/*
+const StyledLink = style(Link)`
+    text-decoration: none;
+    font-size: 20px;
+    padding: 10px;
+    padding-right: 20px;
+    padding-left: 20px;
+    background-color: white;
+    color: black;
+    font-weight: 100;
+`;*/
+
+function Navigation(){
+    return (<div className = "menu">
+        <>
+        <Link className = "links" to ="/">Home</Link>
+        <Link className = "links" to = "/about">About</Link>
+        </>    
+    </div>
+    );
+}
+
+export default Navigation;
